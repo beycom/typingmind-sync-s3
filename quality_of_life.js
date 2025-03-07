cb819edc3bd04ebc36827a71763965fae5b4f2a6
0xCope <0xcope@gmail.com>
2025-03-07 23:28:28 +0100

    .

--- quality_of_life.js
+++ quality_of_life.js
@@ -1,2 +1,8 @@
 
-setTimeout(() => {document.querySelector("[data-element-id=workspace-tab-teams]").remove()}, 1000);
+function removeShitFromUI() {
+    document.querySelector("[data-element-id=workspace-tab-teams]").remove()
+    document.getElementsByClassName("text-white text-xs font-semibold bg-red-700 rounded-sm px-0.5 py-0.5 cursor-default hover:bg-red-600 transition-colors flex items-center justify-center gap-1 truncate absolute top-1 md:top-0 right-0")[0]
+}
+
+
+setTimeout(() => {removeShitFromUI}, 500);
