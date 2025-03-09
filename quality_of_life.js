008575a611bd2c4d5ea9412a66ad1ac49f21d701
0xCope <0xcope@gmail.com>
2025-03-09 14:19:01 +0100

    .

--- quality_of_life.js
+++ quality_of_life.js
@@ -1,5 +1,6 @@
 
 function removeShitFromUI() {
+    console.log("Enhancing UI")
     document.querySelector("[data-element-id=workspace-tab-teams]").remove()
     document.getElementsByClassName("text-white text-xs font-semibold bg-red-700 rounded-sm px-0.5 py-0.5 cursor-default hover:bg-red-600 transition-colors flex items-center justify-center gap-1 truncate absolute top-1 md:top-0 right-0")[0]
 }
