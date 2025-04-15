f92ef5332597b2e4a7cdeeca2cd867be04df53e3
BC <115327482+beycom@users.noreply.github.com>
2025-04-15 23:11:47 +1000

    chore(init): create project by forking typingmind-sync-s3

--- quality_of_life.js
+++ quality_of_life.js
@@ -1,9 +1,11 @@
-
 function removeShitFromUI() {
-    console.log("Enhancing UI")
-    document.querySelector("[data-element-id=workspace-tab-teams]").remove()
-    document.getElementsByClassName("text-white text-xs font-semibold bg-red-700 rounded-sm px-0.5 py-0.5 cursor-default hover:bg-red-600 transition-colors flex items-center justify-center gap-1 truncate absolute top-1 md:top-0 right-0")[0]
+  console.log("Enhancing UI");
+  document.querySelector("[data-element-id=workspace-tab-teams]").remove();
+  document.getElementsByClassName(
+    "text-white text-xs font-semibold bg-red-700 rounded-sm px-0.5 py-0.5 cursor-default hover:bg-red-600 transition-colors flex items-center justify-center gap-1 truncate absolute top-1 md:top-0 right-0"
+  )[0];
 }
 
-
-setTimeout(() => {removeShitFromUI()}, 500);
+setTimeout(() => {
+  removeShitFromUI();
+}, 500);
