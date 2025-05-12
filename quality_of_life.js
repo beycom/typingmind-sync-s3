182d0dd870c8c159a3dc3a53301ba1c0f40f3dc6
0xCope <105827338+0xCope@users.noreply.github.com>
2025-05-12 23:58:01 +0200

    Update quality_of_life.js

--- quality_of_life.js
+++ quality_of_life.js
@@ -2,7 +2,8 @@
 function removeShitFromUI() {
     console.log("Enhancing UI")
     document.querySelector("[data-element-id=workspace-tab-teams]").remove()
-    document.getElementsByClassName("text-white text-xs font-semibold bg-red-700 rounded-sm px-0.5 py-0.5 cursor-default hover:bg-red-600 transition-colors flex items-center justify-center gap-1 truncate absolute top-1 md:top-0 right-0")[0]
+    document.getElementsByClassName("text-white text-xs font-semibold bg-red-700 rounded-sm px-0.5 py-0.5 cursor-default hover:bg-red-600 transition-colors flex items-center justify-center gap-1 truncate absolute top-1 md:top-0 right-0")[0].remove()
+    document.getElementsByClassName("min-w-[58px] sm:min-w-0 sm:aspect-auto aspect-square cursor-default h-12 md:h-[50px] flex-col justify-start items-start inline-flex focus:outline-0 focus:text-white w-full")[0].remove()
 }
 
 
